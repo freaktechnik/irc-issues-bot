be86ce0bea3c1cc8bbc14a031d5bcb113dfa806b
Martin Giger <martin@humanoids.be>
2016-04-02 08:56:44 +0200

    Say it in the channel

--- eventbot.js
+++ eventbot.js
@@ -35,7 +35,7 @@ function EventBot(client, channel, query) {
 	    that.doStuff();
 	    if(that.event && that.event.start.getTime() <= Date.now() - INTERVAL) {
 	        var startsIn = new Date() - that.event.start;
-	        client.say(that.event.summary+" ("+that.event.url+") starts in "+startsIn.getHours()+" hours and "+startsIn.getMinutes()+" minutes.");
+	        client.say(that.channel, that.event.summary+" ("+that.event.url+") starts in "+startsIn.getHours()+" hours and "+startsIn.getMinutes()+" minutes.");
         }
 	}, INTERVAL);
 	this.doStuff();
