cb0004e5f4faf9002f347c467f70c4b930128a75
Martin Giger <martin@humanoids.be>
2016-07-01 14:50:36 +0200

    Fix event message. Probably. Fixes #5 (for now)

--- eventbot.js
+++ eventbot.js
@@ -36,7 +36,7 @@ function EventBot(client, channel, query) {
 	this.iid = setInterval(function() {
 	    that.doStuff();
         try {
-	        if(that.event && that.event.start.getTime() >= Date.now() && that.event.start.getTime() <= Date.now() - INTERVAL) {
+	        if(that.event && that.event.start.getTime() >= Date.now() && that.event.start.getTime() <= Date.now() + INTERVAL) {
 	            var startsIn = new Date(Date.now() - that.event.start.getTime());
 	            client.say(channel, that.event.summary+" ("+that.event.url+") starts in "+startsIn.getHours()+" hours and "+startsIn.getMinutes()+" minutes.");
             }
