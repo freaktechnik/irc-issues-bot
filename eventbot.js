a7b1c3ecc7ae6dc5991c303a6fbd185585613391
Martin Giger <martin@humanoids.be>
2016-09-05 19:02:00 +0200

    Fix start time announcement time

--- eventbot.js
+++ eventbot.js
@@ -37,7 +37,7 @@ function EventBot(client, channel, query) {
 	    that.doStuff();
         try {
 	        if(that.event && that.event.start.getTime() >= Date.now() && that.event.start.getTime() <= Date.now() + INTERVAL) {
-	            var startsIn = new Date(Date.now() - that.event.start.getTime());
+	            var startsIn = new Date(that.event.start.getTime() - Date.now());
 	            client.say(channel, that.event.summary+" ("+that.event.url+") starts in "+startsIn.getHours()+" hours and "+startsIn.getMinutes()+" minutes.");
             }
         } catch(e) {
